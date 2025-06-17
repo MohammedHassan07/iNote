43b5dd9cf945c91eae751fc6841b3c8af9dbde2b
MohammedHassan07 <mhassanch260@gmail.com>
2025-06-17 23:32:37 +0530

    FEAT: get the title and noteContent

--- app/Note.jsx
+++ app/Note.jsx
@@ -1,13 +1,31 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { Platform, ScrollView, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ColourPalette from '../components/ColourPalette';
 
 const Note = () => {
 
+  const [noteContent, setNoteContent] = useState('')
+  const [noteTitle, setNoteTitle] = useState('')
+  const iOS = Platform.OS == 'ios'
+  const [bgColour, setBgcolour] = useState('blue')
+  const navigation = useNavigation()
 
   const colourCode = ['red', 'blue', 'yellow', 'green', 'neutral', 'purple', 'lime', 'amber'];
+
+  // useEffect is used becuase it avoids memory leaks due to the event listener we added to the on going back
+  useEffect(() => {
+
+    const unsubscribe =
+      navigation.addListener('beforeRemove', async () => {
+
+        await saveNoteData(noteTitle, noteContent)
+      })
+    return unsubscribe
+  }, [navigation, noteTitle, noteContent])
+
 
   const getBorderColor = (color) => {
     // console.log(color)
@@ -38,14 +56,25 @@ const Note = () => {
     return map[color]
   }
 
-  const iOS = Platform.OS == 'ios'
-
-  const [bgColour, setBgcolour] = useState('blue')
-
   const handleBgChange = (code) => {
 
     setBgcolour(code)
     console.log('pressed bgChange', code)
+  }
+
+  const handleNoteData = async () => {
+
+    await saveNoteData(noteTitle, noteContent)
+  }
+
+  const saveNoteData = async (noteTitle, noteContent) => {
+    try {
+
+      console.log('clicked tick', noteTitle, noteContent)
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -53,41 +82,18 @@ const Note = () => {
       style={{ backgroundColor: getBodyColor(bgColour) }}
       className={'flex-1'}>
 
+
       {/* header for Note screen tick mark to save the note,  */}
       {/* logo */}
       <View className={`flex-row items-center justify-between mx-3 ${iOS ? '-mb-2' : 'mb-3'}`}>
         <Text className='text-3xl text-black'>
           <Text className='text-5xl font-semibold'>i</Text>Note
         </Text>
-        <MaterialCommunityIcons name="check" size={30} color="black" />
+        <MaterialCommunityIcons onPress={handleNoteData} name="check" size={30} color="black" />
       </View>
 
-      {/* Title input */}
-      <TextInput
-        className={`border-2 mx-4 rounded-full px-3 text-xl text-black`}
-        placeholder='Title'
-        style={{borderColor: 'black'}}
-        // style={{ borderColor: getBorderColor(bgColour) }}
-      />
-
-      {/* Note content */}
-      <ScrollView
-
-        className={'border-t-2 mx-4 mt-2 '}
-        style={{borderColor: 'black'}}
-        // style={{ borderTopColor: getBorderColor(bgColour) }}
-        horizontal={false}
-      >
-
-        <TextInput
-          multiline={true}
-          placeholder='Note here ...'
-          className='text-lg text-black'
-        />
-      </ScrollView>
-
       {/* Colour Paletter */}
-      <View className='flex-row justify-start items-center gap-4 mx-4'>
+      <View className='flex-row justify-between items-center gap-4 mx-4 mb-2'>
 
         {colourCode.map((code, index) => <ColourPalette
           key={index}
@@ -96,6 +102,32 @@ const Note = () => {
         }
 
       </View>
+
+      {/* Title input */}
+      <TextInput
+        className={`border-2 mx-4 rounded-full px-3 text-xl text-black`}
+        placeholder='Title'
+        style={{ borderColor: getBorderColor(bgColour) }}
+        onChangeText={(text) => { setNoteTitle(text) }}
+      />
+
+      {/* Note content */}
+      <ScrollView
+
+        className={'border-t-2 mx-4 mt-2 '}
+        style={{ borderTopColor: getBorderColor(bgColour) }}
+        horizontal={false}
+      >
+
+        <TextInput
+          multiline={true}
+          placeholder='Note here ...'
+          className='text-lg text-black h-screen'
+          style={{ textAlignVertical: 'top' }}
+          onChangeText={(text) => { setNoteContent(text) }}
+        />
+      </ScrollView>
+
 
     </SafeAreaView>
   )
